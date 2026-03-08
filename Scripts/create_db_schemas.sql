5fcaf4b8f5c3c92b578e462c498c453bcc502960
raihantitas <62028009+raihantitas@users.noreply.github.com>
2026-03-09 00:49:50 +0600

    Create 'VincentTest' database and schemas
    This script creates the 'VincentTest' database and its associated schemas: bronze, silver, and gold.

---
+++ Scripts/create_db_schemas.sql
@@ -0,0 +1,19 @@
+Use master;
+Go
+
+-- Create the 'VincentTest' database
+Create Database VincentTest;
+Go
+
+Use VincentTest;
+Go
+
+
+--Create the Schemas 
+Create Schema bronze;
+Go
+
+Create Schema silver;
+Go
+
+Create Schema gold;
